931fa31505fc31c710f797cdb6d9acc387af23b4
Martin Tomov <mtomov@outlook.com>
2014-03-20 19:32:47 +0000

    Removing duplicate code

--- app/controllers/spree/user_registrations_controller.rb
+++ app/controllers/spree/user_registrations_controller.rb
@@ -22,21 +22,6 @@ class Spree::UserRegistrationsController < Devise::RegistrationsController
 
   # POST /resource/sign_up
   def create
-    @user = build_resource(spree_user_params)
-    if resource.save
-      set_flash_message(:notice, :signed_up)
-      sign_in(:spree_user, @user)
-      session[:spree_user_signup] = true
-      associate_user
-      sign_in_and_redirect(:spree_user, @user)
-    else
-      clean_up_passwords(resource)
-      render :new
-    end
-  end
-
-  # POST /resource/sign_up
-  def create
     @user = Spree::User.where(email: params[:spree_user][:email], enrolled: false).first
     if @user
       @user.assign_attributes(spree_user_params)
