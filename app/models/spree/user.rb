3ada48b9394dca31291c0370fb99747d5aa48dec
David Dawson <david.dawson@gmail.com>
2014-04-15 18:16:13 +0100

    guest user tracking
    Conflicts:
    	app/models/spree/user.rb
    	lib/controllers/frontend/spree/checkout_controller_decorator.rb

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -6,20 +6,32 @@ module Spree
     end
 
     devise :database_authenticatable, :registerable, :recoverable,
-           :rememberable, :trackable, :validatable, :encryptable, :encryptor => 'authlogic_sha512'
+           :rememberable, :trackable, :encryptable, :encryptor => 'authlogic_sha512'
 
     has_many :orders
 
     before_validation :set_login
     before_destroy :check_completed_orders
+
     before_validation do
       self.uuid ||= Spree::User.generate_token(:uuid) if self.respond_to?(:uuid)
     end
+
+        validates :uuid, uniqueness: true
+    validates_presence_of   :email, :if => :email_required?
+    validates_uniqueness_of :email, :allow_blank => true,  scope: :enrolled, :if => :email_changed?
+    validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+    
+    validates_presence_of     :password, :if => :password_required?
+    validates_confirmation_of :password, :if => :password_required?
+    validates_length_of       :password, :within => Devise.password_length, :allow_blank => true
 
     users_table_name = User.table_name
     roles_table_name = Role.table_name
 
     scope :admin, -> { includes(:spree_roles).where("#{roles_table_name}.name" => "admin") }
+    scope :registered, -> { where("#{users_table_name}.email NOT LIKE ?", "%@example.net") }
+    scope :not_enrolled, -> { where(enrolled: false) }
 
     class DestroyWithOrdersError < StandardError; end
 
@@ -27,24 +39,40 @@ module Spree
       User.admin.count > 0
     end
 
+
+    def self.create_unenrolled(opts={})
+      salt = generate_token(:password_salt)
+      u = new(email: opts[:email], uuid: opts[:uuid], enrolled: false, password_salt: salt)
+      u.save
+      u
+    end
+
     def admin?
       has_spree_role?('admin')
     end
 
     protected
-      def password_required?
+    def password_required?
+      if self.enrolled
         !persisted? || password.present? || password_confirmation.present?
+      else
+        false
       end
+    end
+
+    def email_required?
+      true
+    end
 
     private
 
-      def check_completed_orders
-        raise DestroyWithOrdersError if orders.complete.present?
-      end
+    def check_completed_orders
+      raise DestroyWithOrdersError if orders.complete.present?
+    end
 
-      def set_login
-        # for now force login to be same as email, eventually we will make this configurable, etc.
-        self.login ||= self.email if self.email
-      end
+    def set_login
+      # for now force login to be same as email, eventually we will make this configurable, etc.
+      self.login ||= self.email if self.email
+    end
   end
 end
--- lib/controllers/frontend/spree/checkout_controller_decorator.rb
+++ lib/controllers/frontend/spree/checkout_controller_decorator.rb
@@ -19,34 +19,42 @@ Spree::CheckoutController.class_eval do
   end
 
   private
-    def order_params
-      if params[:order]
-        params.require(:order).permit(:email)
-      else
-        {}
-      end
+  def find_or_create_user(email)
+    _user  = Spree::User.find_by_email(email)
+    if _user.blank?
+      _user = Spree::User.create_unenrolled(email: email, uuid: tracking_cookie)
     end
+    _user
+  end
 
-    def skip_state_validation?
-      %w(registration update_registration).include?(params[:action])
+  def order_params
+    if params[:order]
+      params.require(:order).permit(:email).merge(user_id: @user.id, created_by_id: @user.id)
+    else
+      {}
     end
+  end
 
-    def check_authorization
-      authorize!(:edit, current_order, session[:access_token])
-    end
+  def skip_state_validation?
+    %w(registration update_registration).include?(params[:action])
+  end
 
-    # Introduces a registration step whenever the +registration_step+ preference is true.
-    def check_registration
-      return unless Spree::Auth::Config[:registration_step]
-      return if spree_current_user or current_order.email
-      store_location
-      redirect_to spree.checkout_registration_path
-    end
+  def check_authorization
+    authorize!(:edit, current_order, session[:access_token])
+  end
 
-    # Overrides the equivalent method defined in Spree::Core.  This variation of the method will ensure that users
-    # are redirected to the tokenized order url unless authenticated as a registered user.
-    def completion_route
-      return order_path(@order) if spree_current_user
-      spree.token_order_path(@order, @order.token)
-    end
+  # Introduces a registration step whenever the +registration_step+ preference is true.
+  def check_registration
+    return unless Spree::Auth::Config[:registration_step]
+    return if spree_current_user or current_order.email
+    store_location
+    redirect_to spree.checkout_registration_path
+  end
+
+  # Overrides the equivalent method defined in Spree::Core.  This variation of the method will ensure that users
+  # are redirected to the tokenized order url unless authenticated as a registered user.
+  def completion_route
+    return order_path(@order) if spree_current_user
+    spree.token_order_path(@order, @order.token)
+  end
 end
