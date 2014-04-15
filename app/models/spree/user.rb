4b3c2f51566ce5bff08a0546baadf9089f218555
David Dawson <david.dawson@gmail.com>
2014-04-15 18:21:15 +0100

    Remove Core::UserBanners
    Conflicts:
    	app/models/spree/user.rb

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -17,11 +17,11 @@ module Spree
       self.uuid ||= Spree::User.generate_token(:uuid) if self.respond_to?(:uuid)
     end
 
-        validates :uuid, uniqueness: true
+    validates :uuid, uniqueness: true
     validates_presence_of   :email, :if => :email_required?
     validates_uniqueness_of :email, :allow_blank => true,  scope: :enrolled, :if => :email_changed?
     validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
-    
+
     validates_presence_of     :password, :if => :password_required?
     validates_confirmation_of :password, :if => :password_required?
     validates_length_of       :password, :within => Devise.password_length, :allow_blank => true
