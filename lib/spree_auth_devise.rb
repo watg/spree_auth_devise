7873b6327fc96e81d118e6afcf85e972792fa105
David Dawson <david.dawson@gmail.com>
2015-01-16 10:44:27 +0000

    fix unmerged file

--- lib/spree_auth_devise.rb
+++ lib/spree_auth_devise.rb
@@ -1,9 +1,5 @@
 require 'spree_core'
 require 'spree/auth/devise'
 require 'spree/authentication_helpers'
-<<<<<<< HEAD
-require 'coffee_script'
-=======
 require 'sass/rails'
 require 'coffee_script'
->>>>>>> upstream/2-4-stable
